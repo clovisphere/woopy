227e40974cc5c76663a65e1a0cef66fee32ffaab
clovisphere <clovis.mugaruka@gmail.com>
2023-09-30 16:51:32 +0300

    Define a snackbar module, and put all the state+behavior related to snackbar in it

---
+++ flicks/lib/flicks/snackbar.rb
@@ -0,0 +1,18 @@
+module Snackbar
+
+    # define a struct
+    Snack = Struct.new(:name, :price)
+
+    # 👉🏽 use a Data class (https://docs.ruby-lang.org/en/3.2/Data.html) instead (ruby >= 3.2)
+    #Snack = Data.define(:name, :price)
+
+    SNACKS = [
+        Snack.new("🍪", 2),
+        Snack.new("🍫", 5),
+        Snack.new("🌮", 6),
+        Snack.new("🍦", 3)
+    ]
+
+    def self.random_snack = SNACKS.sample
+
+end
