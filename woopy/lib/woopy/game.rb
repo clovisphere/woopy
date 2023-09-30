862ac9ec8f36183c97cabacf428b15e62b0db89b
clovisphere <clovis.mugaruka@gmail.com>
2023-09-30 16:37:32 +0300

    Add a 'treasure' struct or data class

--- woopy/lib/woopy/game.rb
+++ woopy/lib/woopy/game.rb
@@ -1,4 +1,21 @@
 class Game
+
+    # define a struct
+    Treasure = Struct.new(:name, :points)
+
+    # 👉🏽 use a Data class (https://docs.ruby-lang.org/en/3.2/Data.html) instead (ruby >= 3.2)
+    #Treasure = Data.define(:name, :price)
+
+    TREASURES = [
+        Treasure.new("🥧", 10),
+        Treasure.new("🪙", 25),
+        Treasure.new("🪈", 50),
+        Treasure.new("🧭", 65),
+        Treasure.new("🔑", 80),
+        Treasure.new("👑", 90),
+        Treasure.new("⭐️", 100)
+    ]
+
     attr_reader :title, :players
 
     def initialize(title)
@@ -9,7 +26,10 @@ class Game
     def add_player(player) = @players << player
 
     def play(rounds = 1)
-        puts "Before playing:"
+        puts "\nThe following treasures can be found:"
+        TREASURES.each { |treasure| puts "- #{treasure.name} is worth #{treasure.points} points" }
+
+        puts "\nBefore playing:"
         puts @players
 
         1.upto(rounds) do |round|
@@ -26,6 +46,8 @@ class Game
                     player.boost
                     puts "#{player.name} got boosted 😺"
                 end
+                treasure = TREASURES.sample
+                puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points\n\n"
             end
         end
 
